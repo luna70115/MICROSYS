d8549cf9217afd132402325d500dcaa2cb7756cc
Baguaii <baguaii@BaguaiideMacBook-Air.local>
2025-06-26 19:07:13 +0800

    建立檔案

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import "./assets/styles/main.scss";
+
+const now = new Date();
+
+function formatCustomDate(date: Date): string {
+  const pad = (n: number) => n.toString().padStart(2, "0");
+
+  const month = pad(date.getMonth() + 1);
+  const day = pad(date.getDate());
+  const year = date.getFullYear();
+
+  const hours = pad(date.getHours());
+  const minutes = pad(date.getMinutes());
+  const seconds = pad(date.getSeconds());
+
+  return `${month}/${day}/${year} ${hours} ${minutes}:${seconds}`;
+}
+
+const formattedNow = formatCustomDate(now);
+</script>
+
+<template>
+
+
+
+   
+
+</template>
+
+<style lang="scss">
+
+</style>

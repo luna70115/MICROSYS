6146067b218ab4424102bea1811dad844cb12c1e
“Luna” <luna_su@example.com>
2025-06-27 11:33:01 +0800

    最終版

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,7 @@
 import SideBar from "./components/sideBar.vue";
 import Header from "./components/header.vue";
 import Content from "./components/content.vue";
-
 import "./assets/styles/main.scss";
-
-const now = new Date();
-
-function formatCustomDate(date: Date): string {
-  const pad = (n: number) => n.toString().padStart(2, "0");
-
-  const month = pad(date.getMonth() + 1);
-  const day = pad(date.getDate());
-  const year = date.getFullYear();
-
-  const hours = pad(date.getHours());
-  const minutes = pad(date.getMinutes());
-  const seconds = pad(date.getSeconds());
-
-  return `${month}/${day}/${year} ${hours} ${minutes}:${seconds}`;
-}
-
-const formattedNow = formatCustomDate(now);
 </script>
 
 <template>
